6b6395f84507eac23e3668cdb276ed4c842835f8
nawfel <sghaier.nawfel@hotmail.com>
2023-01-22 01:01:20 +0100

    auth guard, services, env updated

--- BlogLab-UI/src/app/services/blog.service.ts
+++ BlogLab-UI/src/app/services/blog.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogCreate } from '../models/blog/blog-create.model';
+import { BlogPaging } from '../models/blog/blog-paging.model';
+import { Blog } from '../models/blog/blog.model';
+import { PagedResult } from '../models/blog/paged-result.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  create(model: BlogCreate): Observable<Blog> {
+    return this.http.post<Blog>(`${environment.webapi}/blog`, model);
+  }
+
+
+
+  getAll(blogPaging: BlogPaging): Observable<PagedResult<Blog>> {
+
+    return this.http.get<PagedResult<Blog>>(`${environment.webapi}/Blog?page=${blogPaging.page}&PageSize=${blogPaging.pageSize}`);
+  }
+
+  get(blogId: number): Observable<Blog> {
+    return this.http.get<Blog>(`${environment.webapi}/Blog/${blogId}`);
+  }
+
+  getByApplicationUserId(applicationUserId:number):Observable<Blog[]> {
+    return this.http.get<Blog[]>(`${environment.webapi}/Blog/${applicationUserId}`);
+
+  }
+
+  getAllFamous():Observable<Blog[]>{
+
+    return this.http.get<Blog[]>(`${environment.webapi}/Blog/famous`);
+  }
+
+  delete(blogId:number):Observable<number>{
+    return this.http.delete<number>(`${environment.webapi}/blog/${blogId}`);
+  }
 }
--- BlogLab-UI/src/app/services/photo.service.ts
+++ BlogLab-UI/src/app/services/photo.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogCreate } from '../models/blog/blog-create.model';
+import { Photo } from '../models/photo/photo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(model :FormData):Observable<Photo>{
+    return this.http.post<Photo>(`${environment.webapi}/photo`,model);
+
+  }
+  getByApplicationUserId():Observable<Photo[]>{
+    return this.http.get<Photo[]>(`${environment.webapi}/photo`);
+
+
+  }
+  get(photoId:BlogCreate):Observable<Photo>{
+    return this.http.get<Photo>(`${environment.webapi}/photo/${photoId}`);
+  }
+  delete(photoId:number):Observable<number>{
+    return this.http.delete<number>(`${environment.webapi}/photo/${photoId}`);
+
+  }
+
 }
